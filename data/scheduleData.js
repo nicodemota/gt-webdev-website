e03342e14e381f1bebb375dfb6fe1b3a6af9bb5b
saribek-hovakimian <h.saribek@gmail.com>
2021-08-27 11:48:23 -0400

    Add temp schedule for fall 2021

--- data/scheduleData.js
+++ data/scheduleData.js
@@ -2,6 +2,59 @@ let meetData = "We meet every Tuesday from 6:30 P.M. to around 7:30 P.M. Atlanta
 
 let scheduleData = [
     {
+        "date": "9/7/2021",
+        "agenda": "First Meeting and Club Overview",
+        "link": "TBD"
+    },
+    {
+        "date": "9/14/2021",
+        "agenda": "Form Project Teams & HTML/CSS tutorial",
+        "link": "TBD"
+    },
+    {
+        "date": "9/21/2021",
+        "agenda": "Project meetings & JavaScript tutorial",
+        "link": "TBD"
+    },
+    {
+        "date": "9/28/2021",
+        "agenda": "Project meetings & React.js tutorial",
+        "link": "TBD"
+    },
+    {
+        "date": "10/4/2021",
+        "agenda": "Project meetings & Node and Express.js tutorial",
+        "link": "TBD"
+    },
+    {
+        "date": "10/11/2021",
+        "agenda": "Project meetings & MongoDB tutorial",
+        "link": "TBD"
+    },
+    {
+        "date": "10/18/2021",
+        "agenda": "Project meetings & Material-UI tutorial",
+        "link": "TBD"
+    },
+    {
+        "date": "10/25/2021",
+        "agenda": "Project meeting & Git/GitHub tutorial",
+        "link": "TBD"
+    },
+    {
+        "date": "11/1/2021",
+        "agenda": "Project meeting & Python Flask tutorial",
+        "link": "TBD"
+    },
+    {
+        "date": "11/8/2021",
+        "agenda": "Project meeting & Next.js tutorial",
+        "link": "TBD"
+    }
+];
+
+let spring2021ScheduleData = [
+    {
         "date": "1/26/2021",
         "agenda": "Project Introductions",
         "link": "?"
