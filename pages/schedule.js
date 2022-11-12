3937b0db90c0d844321ad0e04cde61f5cf6031e1
steveshi-dotcom <st3v5.s2i@gmail.com>
2022-11-12 18:58:35 -0500

    deleted some files

--- pages/schedule.js
+++ pages/schedule.js
@@ -2,8 +2,8 @@ import React, {Component} from "react";
 import NavBar from "../components/NavBar";
 import BottomBar from "../components/BottomBar";
 import Head from 'next/head'
-import {meetData, scheduleData} from '../data/scheduleData.js';
-import {Box, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography} from "@mui/material";
+import {scheduleData} from '../data/scheduleData.js';
+import { Grid } from "@mui/material";
 
 const Schedule = () => {
 
@@ -12,7 +12,7 @@ const Schedule = () => {
         console.log(`${res[0]}\n${res[1]}\n${res[2]}`)
         return `${res[0]}\n${res[1]}\n${res[2]}`
     }
-    
+
     return(
         <div>
             <Head>
@@ -40,65 +40,6 @@ const Schedule = () => {
             <BottomBar />
         </div>
     )
-}
-
-// class Schedule extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <Head>
-//                     <title>Schedule | GT WebDev</title>
-//                 </Head>
-//                 <NavBar />
-//                 <div className="main">
-//                     <Typography variant="h4" className="schedLabel">schedule</Typography>
-//                     <SchedTable />
-//                 </div>
-//                 <BottomBar />
-//             </div>
-//         );
-//     }
-// }
-
-class SchedTable extends Component {
-    render() {
-        return (
-            <TableContainer component={Paper}>
-                <Table>
-                    <TableHead>
-                        <TableRow className="schedTableHeader">
-                            <TableCell align="center" className="schedHeaderCell">
-                                <Box fontWeight={600} fontSize={16}>Meeting Date</Box>
-                            </TableCell>
-                            <TableCell align="center" className="schedHeaderCell">
-                                <Box fontWeight={600} fontSize={16}>Agenda</Box>
-                            </TableCell>
-                            <TableCell align="center" className="schedHeaderCell">
-                                <Box fontWeight={600} fontSize={16}>Recording</Box>
-                            </TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                    {scheduleData.map(row => (
-                        <TableRow key={row.name} className="schedTableRow">
-                            <TableCell align="center" className="schedTableCell">{row.date}</TableCell>
-                            <TableCell align="center" className="schedTableCell">{row.agenda}</TableCell>
-                            <TableCell align="center" className="schedTableCell">
-                                {(row.link === "TBD" || row.link === "N/A") ? row.link
-                                :
-                                    <>
-                                        <a href={row.link} style={{ display: "block" }} target="_blank">Here</a>
-                                        {row.link2 ? <a href={row.link2} style={{ display: "block" }} target="_blank">Here</a> : ""}
-                                    </>
-                                }
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        );
-    }
 }
 
 export default Schedule;
