fef22a05afa665d485706c3d2aaf5241850784cd
steveshi-dotcom <st3v5.s2i@gmail.com>
2022-12-03 18:19:15 -0500

    skeleton of aboutUs page

---
+++ data/aboutusData.js
@@ -0,0 +1,8 @@
+let aboutUs = [
+    "Interactive approach to to learn web development",
+    "project teams use up-to-date technology to make exciting web applications",
+    "Hackathons and other interactivie events where members can pratice their skills as well as gain new ones",
+    "And... workshops where members can learn about cool things like new technologies, design patterns, life after college, etc"
+];
+
+export { aboutUs };
---
+++ pages/aboutus.js
@@ -0,0 +1,57 @@
+import React, {Component} from "react";
+import NavBar from "../components/NavBar";
+import BottomBar from "../components/BottomBar";
+import Head from 'next/head'
+import { aboutUs } from "../data/aboutusData";
+import { Grid, Typography } from "@mui/material";
+
+const AboutUs = () => {
+
+    return(
+        <div>
+            <Head />
+            <NavBar />
+            <Grid container rowGap={7.5} className="aboutUsContainer">
+                <Grid item xs={12} sm={6} md={6}>
+                    <Typography variant="h3" className="aboutUsTxt">
+                        { aboutUs[0] }
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} sm={6} md={6} className="aboutUsGrid">
+                    <img src="undraw-graphic-1.png" className="aboutUsImg"/>
+                </Grid>
+
+                <Grid item xs={12} sm={6} md={6} className="aboutUsGrid">
+                    <img src="undraw-graphic-2.png" className="aboutUsImg"/>
+                </Grid>
+                <Grid item xs={12} sm={6} md={6}>
+                    <Typography variant="h3" className="aboutUsTxt">
+                        { aboutUs[1] }
+                    </Typography>
+                </Grid>
+
+                <Grid item xs={12} sm={6} md={6}>
+                    <Typography variant="h3" className="aboutUsTxt">
+                        { aboutUs[2] }
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} sm={6} md={6} className="aboutUsGrid">
+                    <img src="undraw-graphic-3.png" className="aboutUsImg"/>
+                </Grid>
+
+                <Grid item xs={12} sm={6} md={6} className="aboutUsGrid">
+                    <img src="undraw-graphic-4.png" className="aboutUsImg"/>
+                </Grid>
+                <Grid item xs={12} sm={6} md={6}>
+                    <Typography variant="h3" className="aboutUsTxt">
+                        { aboutUs[3] }
+                    </Typography>
+                </Grid>
+            </Grid>
+            <BottomBar />
+        </div>
+    )
+
+}
+
+export default AboutUs;
