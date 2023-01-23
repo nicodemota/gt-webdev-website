988e2debe4282db9d2f4f93f18ca1cda7a51d97a
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-01-23 02:12:13 -0500

    Calculate groupings of people with their top choice project

--- pages/admin-dashboard.js
+++ pages/admin-dashboard.js
@@ -5,6 +5,7 @@ import {Box, Button, TextareaAutosize, TextField} from "@mui/material";
 import {getAuth, signInWithEmailAndPassword} from 'firebase/auth';
 import firebaseApp from "../helpers/firebase";
 import {useRouter} from 'next/router'
+import {readRemoteFile} from "react-papaparse";
 
 const AdminDashboard = () => {
     const [adminConfirmed, setAdminConfirmed] = useState(false);
@@ -12,7 +13,11 @@ const AdminDashboard = () => {
     // auto-sorting tool
     const [sheetId, setSheetId] = useState('1l4CCiWWuz0y40qaZ9rxIw2tqL-wb-N0VJKrEbtoQWIw');
     const [sheetName, setSheetName] = useState('Form Responses 2');
-    const [applicantSortResult, setApplicantSortResult] = useState('');
+    const [nameColumn, setNameColumn] = useState('B');
+    const [projectColumnStart, setProjectColumnStart] = useState('I');
+    const [projectColumnEnd, setProjectColumnEnd] = useState('Q');
+    const [projectNameRegex, setProjectNameRegex] = useState(`\\[([^\\]]+)]`);
+    const [projectRegexGroup, setProjectRegexGroup] = useState(1);
 
     const auth = getAuth(firebaseApp);
     const router = useRouter();
@@ -42,10 +47,77 @@ const AdminDashboard = () => {
         }
     }, []);
 
+    const excelSheetColumnNumber = function(columnTitle) {
+        // source: https://leetcode.com/problems/excel-sheet-column-number/solutions/1717232/simple-javascript-solution/?q=javascript&orderBy=most_relevant
+        let output = 0;
+        for (let i = 0; i < columnTitle.length; i++) {
+            output += (columnTitle.charCodeAt(columnTitle.length - 1 - i) - 64) * 26**i;
+        }
+        return output - 1;
+    };
+
     const sortApplicants = () => {
         // https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}
         const csvURL = `https://docs.google.com/spreadsheets/d/${encodeURI(sheetId)}/gviz/tq?tqx=out:csv&sheet=${encodeURI(sheetName)}`;
-        window.open(csvURL);
+        readRemoteFile(csvURL, {
+            complete: (results) => {
+                try {
+                    // convert column letters to index (ex: A->0, B->1, etc.)
+                    const nameColumnNumber = excelSheetColumnNumber(nameColumn);
+                    const projectColumnStartNumber = excelSheetColumnNumber(projectColumnStart);
+                    const projectColumnEndNumber = excelSheetColumnNumber(projectColumnEnd);
+
+                    // successfully fetched data of Google Sheet
+                    const data = results?.data;
+                    const formQuestions = data[0];
+
+                    // extract project names and assign them to project groupings
+                    const projectGroupings = {};
+                    for (let column = projectColumnStartNumber; column < projectColumnEndNumber + 1; column++) {
+                        let projectName = formQuestions[column];
+                        const projectNameRegexChecker = new RegExp(projectNameRegex, "g");
+                        const regexResult = projectNameRegexChecker.exec(projectName);
+                        if (regexResult) {
+                            if (regexResult[projectRegexGroup]) {
+                                projectName = regexResult[projectRegexGroup];
+                            } else if (regexResult[0]) {
+                                projectName = regexResult[0];
+                            } else {
+                                console.error("Unable to extract project name from regex result:", regexResult);
+                            }
+                        }
+                        projectGroupings[projectName] = [];
+                    }
+                    const projectNames = Object.keys(projectGroupings);
+
+                    // associate people with projects they chose as their nth preference
+                    for (let row = 1; row < data.length; row++) {
+                        for (let column = projectColumnStartNumber; column < projectColumnEndNumber + 1; column++) {
+                            try {
+                                if (parseInt(data[row][column]) === 1) {
+                                    const projectName = projectNames[column - projectColumnStartNumber];
+                                    console.log(projectName, data[row]);
+                                    projectGroupings[projectName].push(data[row][nameColumnNumber]);
+                                }
+                            } catch (e) {
+                                // could not parse int of data - could be empty or something
+                                // don't do anything cuz we don't care about this case
+                            }
+                        }
+                    }
+
+                    console.log(projectGroupings);
+                } catch (e) {
+                    console.error("Error parsing applicant data: ", e);
+                    alert("Error parsing applicant data. Refer to console logs");
+                }
+            },
+            error(error, _) {
+                // unable to fetch data of Google Sheet
+                console.error(`Unable to download CSV file`, csvURL, error);
+                alert(`ERROR! Ensure Google Sheet can be viewed by anyone. Unable to download CSV file ${csvURL}`);
+            }
+        });
     }
 
     return (
@@ -80,11 +152,23 @@ const AdminDashboard = () => {
                                             Tool to help sort project applicants into teams.
                                             Ensure the Google Sheet is viewable by anyone with the link.
                                         </p>
-                                        <p style={{marginTop: "0px"}}>
+                                        <p style={{marginTop: "0px", wordBreak: "break-all"}}>
                                             {`"Sheet ID" can be found from the URL of the Google Sheet (https://docs.google.com/spreadsheets/d/{Sheet ID})`}
                                         </p>
                                         <p style={{marginTop: "0px"}}>
                                             {`"Sheet Name" is the name of the tab you are on (found at the bottom of Google Sheets)`}
+                                        </p>
+                                        <p style={{marginTop: "0px"}}>
+                                            Name column refers to the column in the Google Sheet that references the preferred name of an applicant
+                                        </p>
+                                        <p style={{marginTop: "0px"}}>
+                                            Project column start and end refer to the columns in the Google Sheet that reference the project preferences results
+                                        </p>
+                                        <p style={{marginTop: "0px"}}>
+                                            {`(Optional) Project name regex will extract the project name from the column title of the Google Sheet`}
+                                        </p>
+                                        <p style={{marginTop: "0px"}}>
+                                            {`(Optional) Project regex group will extract the project name from a specified index of the matched regex group, note this is 0-indexed`}
                                         </p>
                                         <TextField id="outlined-basic" label="Sheet ID" variant="outlined" type="text"
                                                    name="link" fullWidth={true} value={sheetId} onChange={(event) => {
@@ -96,17 +180,31 @@ const AdminDashboard = () => {
                                                    setSheetName(event.target.value)
                                         }}
                                         />
-                                        { (applicantSortResult && applicantSortResult.length > 0) &&
-                                            <>
-                                                <TextField id="outlined-basic" label="CSV Result" variant="outlined" type="text"
-                                                           name="sort-result" fullWidth={true} multiline={true}
-                                                           value={applicantSortResult}
-                                                           onChange={(event) => {
-                                                               setApplicantSortResult(event.target.value)
-                                                           }}/>
-                                                <p>Tip: paste this into Google Sheets</p>
-                                            </>
-                                        }
+                                        <TextField id="outlined-basic" label="Name Column" variant="outlined" type="text"
+                                                   name="link" fullWidth={true} value={nameColumn} onChange={(event) => {
+                                                   setNameColumn(event.target.value)
+                                        }}
+                                        />
+                                        <TextField id="outlined-basic" label="Project Column Start" variant="outlined" type="text"
+                                                   name="link" fullWidth={true} value={projectColumnStart} onChange={(event) => {
+                                                   setProjectColumnStart(event.target.value)
+                                        }}
+                                        />
+                                        <TextField id="outlined-basic" label="Project Column End" variant="outlined" type="text"
+                                                   name="link" fullWidth={true} value={projectColumnEnd} onChange={(event) => {
+                                                   setProjectColumnEnd(event.target.value)
+                                        }}
+                                        />
+                                        <TextField id="outlined-basic" label="Project Name Regex" variant="outlined" type="text"
+                                                   name="link" fullWidth={true} value={projectNameRegex} onChange={(event) => {
+                                                   setProjectNameRegex(event.target.value)
+                                        }}
+                                        />
+                                        <TextField id="outlined-basic" label="Project Regex Group" variant="outlined" type="number"
+                                                   name="link" fullWidth={true} value={projectRegexGroup} onChange={(event) => {
+                                                   setProjectRegexGroup(parseInt(event.target.value))
+                                        }}
+                                        />
                                         <Button onClick={() => {
                                             sortApplicants();
                                         }} variant="contained">✨Sort ✨</Button>
