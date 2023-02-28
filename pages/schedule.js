db87a87de79c3c7063981dbf81b632b91308eeaf
rae <heavenyuh@gmail.com>
2023-02-28 11:37:17 -0500

    Styled schedule page

--- pages/schedule.js
+++ pages/schedule.js
@@ -2,6 +2,8 @@ import React, {Component} from "react";
 import NavBar from "../components/NavBar";
 import BottomBar from "../components/BottomBar";
 import Head from 'next/head'
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 import {scheduleData} from '../data/scheduleData.js';
 import { Grid, Typography } from "@mui/material";
 
@@ -21,32 +23,115 @@ const Schedule = () => {
                 <title>Schedule | GT WebDev</title>
             </Head>
             <NavBar />
+            <VerticalTimeline>
+                <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                contentStyle={{ background: '#f0cd42', color: '#222220' }}
+                contentArrowStyle={{ borderRight: '7px solid #f0cd42' }}
+                date=""
+                // iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                // icon={< WorkIcon />}
+                >
+                <h3 className="vertical-timeline-element-title"> General Board Meeting </h3>
+                <h4 className="vertical-timeline-element-subtitle">  February 24, 2023 - Howey L4</h4>
+                <p>
+                    Project meeting, Git/GitHub tutorial
+                </p>
+                </VerticalTimelineElement>
+
+                <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                contentStyle={{ background: '#f0cd42', color: '#222220' }}
+                contentArrowStyle={{ borderRight: '7px solid #f0cd42' }}
+                date=""
+                // iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                // icon={<WorkIcon />}
+                >
+                <h3 className="vertical-timeline-element-title"> General Board Meeting </h3>
+                <h4 className="vertical-timeline-element-subtitle">  February 24, 2023 - Howey L4</h4>
+                <p>
+                    Project meeting, Git/GitHub tutorial
+                </p>
+                </VerticalTimelineElement>
+
+                <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                contentStyle={{ background: '#f0cd42', color: '#222220' }}
+                contentArrowStyle={{ borderRight: '7px solid #f0cd42' }}
+                date=""
+                // iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                // icon={<WorkIcon />}
+                >
+                <h3 className="vertical-timeline-element-title"> General Board Meeting </h3>
+                <h4 className="vertical-timeline-element-subtitle">  February 24, 2023 - Howey L4</h4>
+                <p>
+                    Project meeting, Git/GitHub tutorial
+                </p>
+                </VerticalTimelineElement>
+               
+                <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                contentStyle={{ background: '#f0cd42', color: '#222220' }}
+                contentArrowStyle={{ borderRight: '7px solid #f0cd42' }}
+                date=""
+                // iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                // icon={<WorkIcon />}
+                >
+                <h3 className="vertical-timeline-element-title"> General Board Meeting </h3>
+                <h4 className="vertical-timeline-element-subtitle">  February 24, 2023 - Howey L4</h4>
+                <p>
+                    Project meeting, Git/GitHub tutorial
+                </p>
+                </VerticalTimelineElement>
+                
+                <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                contentStyle={{ background: '#f0cd42', color: '#222220' }}
+                contentArrowStyle={{ borderRight: '7px solid #f0cd42' }}
+                date=""
+                // iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                // icon={<WorkIcon />}
+                >
+                <h3 className="vertical-timeline-element-title"> General Board Meeting </h3>
+                <h4 className="vertical-timeline-element-subtitle">  February 24, 2023 - Howey L4</h4>
+                <p>
+                    Project meeting, Git/GitHub tutorial
+                </p>
+                </VerticalTimelineElement>
+                
+                <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                contentStyle={{ background: '#f0cd42', color: '#222220' }}
+                contentArrowStyle={{ borderRight: '7px solid #f0cd42' }}
+                date=""
+                // iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                // icon={<WorkIcon />}
+                >
+                <h3 className="vertical-timeline-element-title"> General Board Meeting </h3>
+                <h4 className="vertical-timeline-element-subtitle">  February 24, 2023 - Howey L4</h4>
+                <p>
+                    Project meeting, Git/GitHub tutorial
+                </p>
+                </VerticalTimelineElement>
+                
+                <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                contentStyle={{ background: '#f0cd42', color: '#222220' }}
+                contentArrowStyle={{ borderRight: '7px solid #f0cd42' }}
+                date=""
+                // iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                // icon={<WorkIcon />}
+                >
+                <h3 className="vertical-timeline-element-title"> General Board Meeting </h3>
+                <h4 className="vertical-timeline-element-subtitle">  February 24, 2023 - Howey L4</h4>
+                <p>
+                    Project meeting, Git/GitHub tutorial
+                </p>
+                </VerticalTimelineElement>
+                
+                
+            </VerticalTimeline>
             
-            <Grid container rowGap={3} className="scheduleContainer">
-                {scheduleData.map((cur, ind) => {
-                    return(
-                        <Grid container item className="scheduleGrid">
-                            <Grid xs={4} className="scheduleDateGrid">
-                                <Typography className="scheduleDateText">
-                                    {oneColText(cur['date'])}
-                                </Typography>
-                            </Grid>
-                            <Grid xs={6} >
-                                <Typography className="scheduleAgendaTxt">
-                                    {cur['agenda']}
-                                </Typography>
-                            </Grid>
-                            <Grid xs={2} >
-                                {cur['link'] != 'N/A'
-                                ? 
-                                <a href={cur['link']}> <img src="camera.png" className="bottomBarImg"/></a>
-                                :
-                                <span />}
-                            </Grid>
-                        </Grid>
-                    )
-                })}
-            </Grid>
             <BottomBar />
         </div>
     )
