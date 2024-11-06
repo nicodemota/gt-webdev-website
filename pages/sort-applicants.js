d409f2ea674516f85f01177ed43f6581a37edfeb
Xinxinidea <87244651+Xinxinidea@users.noreply.github.com>
2024-11-06 10:31:28 -0500

    8 connect sort applicants page to firebase db (#18)

--- pages/sort-applicants.js
+++ pages/sort-applicants.js
@@ -1,54 +1,60 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Board from 'react-ui-kanban';
 import AdminNavBar from "../components/AdminNavBar";
 import Head from 'next/head'
 import { Select, MenuItem, FormControl, InputLabel, Button } from "@mui/material";
+import firebaseApp from "../helpers/firebase";
+import { getFirestore, collection, query, where, getDocs } from "firebase/firestore";
 
 function sortMembersPage() {
 
-    function handleClick() {
+    const db = getFirestore(firebaseApp);
+    const [applicants, setApplicants] = useState([]);
+    const [projects, setProjects] = useState([]);
+
+    React.useEffect(async () => {
+        const q = query(collection(db, "applicants"), where("isFormerMember", "==", false));
+        const querySnapshotApplicants = await getDocs(q);
+
+        querySnapshotApplicants.forEach((doc) => {
+            let applicantProjects = "";
+
+            for (let i = 0; i < doc.data().projects.length; i++) {
+                applicantProjects += doc.data().projects[i] + ", ";
+            }
+
+            applicantProjects = applicantProjects.substring(0, applicantProjects.length - 2);
+
+            // setApplicants((applicants) => {
+            //     return [...applicants, {id: doc.id, title: doc.data().name, description: "Is former applicant: " + doc.data().isFormerApplicant + "\nIs former member: " + doc.data().isFormerMember + "\nProjects: " + applicantProjects}]
+            // });
+
+            setApplicants((applicants) => {
+                return [...applicants, {id: doc.id, title: doc.data().name}]
+            })
+        });
+
+        const querySnapshotProjects = await getDocs(collection(db, "projects"));
         
-    }
+        querySnapshotProjects.forEach((doc) => {
+            setProjects((projects) => {
+                return [
+                    ...projects,
+                    {id: doc.id, title: doc.data().name, style: {display: 'flex'}, cards: []}
+                ]
+            });
+        })
+    }, []);
 
     const data = {
-        lanes: [
-            {
-                id: 'lane1',
-                title: 'Project 1',
-                style: { display: "flex" },
-                cards: [
-                    {id: 'card1', title: 'Applicant 1', description: '...', label: '...', onClick: "handleClick"},
-                    {id: 'card2', title: 'Applicant 2', description: '...', label: '...'}
-                ]
-            },
-            {
-                id: 'lane2',
-                title: 'Project 2',
-                style: { display: "flex" },
-                cards: [
-                    {id: 'card3', title: 'Applicant 3', description: '...', label: '...'},
-                    {id: 'card4', title: 'Applicant 4', description: '...', label: '...'}
-                ]
-            },
-            {
-                id: 'lane3',
-                title: 'Project 3',
-                style: { display: "flex" },
-                cards: [
-                    {id: 'card3', title: 'Applicant 3', description: '...', label: '...'},
-                    {id: 'card4', title: 'Applicant 4', description: '...', label: '...'}
-                ]
-            },
+        lanes: projects.concat(
             {
                 id: 'lane4',
                 title: 'Not Accepted',
                 style: { backgroundColor: '#F08080', display: "flex" },
-                cards: [
-                    {id: 'card5', title: 'Applicant 5', description: '...', label: '...'},
-                    {id: 'card6', title: 'Applicant 6', description: '...', label: '...'}
-                ]
+                cards: applicants,
             }
-        ]
+        )
     }
 
     return (
@@ -62,12 +68,16 @@ function sortMembersPage() {
                 <FormControl>
                     <InputLabel id="select-label">Semester</InputLabel>
                     <Select labelId="select-label" label="Semester" style={{marginBottom: '10px', width: "200px"}}>
-                        <MenuItem>Fall 2023</MenuItem>
-                        <MenuItem>Spring 2023</MenuItem>
+                        <MenuItem children="Fall 2023">Fall 2023</MenuItem>
+                        <MenuItem children="Spring 2023">Spring 2023</MenuItem>
                     </Select>
                 </FormControl>
                 <div>
-                    <Board data={data} draggable="true" style={{overflowY: "scroll", backgroundColor: 'gray'}}></Board>
+                    <Board
+                        data={data}
+                        draggable="true"
+                        style={{overflowY: "scroll", backgroundColor: 'gray'}}
+                    />
                 </div>
                 <div style={{margin: "10px auto", width: "35%", display: "flex", flexDirection: "column", gap: "10px", justifyContent: "center"}}>
                     <Button variant="contained">Generate Rejection Email List</Button>
@@ -77,6 +87,12 @@ function sortMembersPage() {
             </div>
         </div>
     )
+}
+
+function applicantModal() {
+    return <div>
+        <p>Applicant</p>
+    </div>
 }
 
 export default sortMembersPage;
